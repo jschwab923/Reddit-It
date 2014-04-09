3f1d2ec43bef4590995b4c4311da7dc5e598ee22
Jeff Schwab <jschwab923@gmail.com>
2014-04-09 10:52:26 -0700

    Added RedditPost class

---
+++ Reddit It/JWCRedditPost.h
@@ -0,0 +1,13 @@
+//
+//  JWCRedditPost.h
+//  Reddit It
+//
+//  Created by Jeff Schwab on 4/9/14.
+//  Copyright (c) 2014 Jeff Writes Code. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JWCRedditPost : NSObject
+
+@end
